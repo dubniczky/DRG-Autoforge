1af36d599109efd1ec763a875e08bb267ca9ac66
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-27 18:11:04 +0100

    Added initial forge script version for 4K

---
+++ forge.py
@@ -0,0 +1,32 @@
+import pyautogui as gui
+from time import sleep
+import keyboard
+
+width, height = gui.size()
+
+print(gui.position())
+
+# top core position on screen: Point(x=2795, y=761)
+# start craft position on screen: Point(x=1907, y=1327)
+
+def forgeLoop():
+    # Select item to craft
+    gui.moveTo(2795, 761, duration=0.25)
+    gui.click()
+    
+    # Start crafitng
+    gui.moveTo(1907, 1327, duration=0.25)
+    gui.click()
+    
+    sleep(18.0)
+    
+    gui.moveTo(1907, 1327, duration=0.25)
+    gui.click()
+    
+    sleep(3.0)
+    
+    
+for i in range(0, 100):
+    if keyboard.is_pressed('q'):
+        break
+    forgeLoop()
